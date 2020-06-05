5e18a518c8bf580f4c5309ef30474bcfced4ca55
Carlos <csuescun@gmail.com>
2020-06-04 21:45:54 -0500

    new user

--- js/controller/login.js
+++ js/controller/login.js
@@ -3,8 +3,13 @@ app.controller('loginController', function($scope, apiInterface, snackbar, userI
   // $('footer').remove();
   $scope.loginData = {};
   $scope.errorData = {};
-  $scope.user = {};
   
+  if(userInterface.auth()){
+    $scope.user = userInterface.getUser();
+  }
+  else{
+    $scope.user = {};
+  }
 
   $scope.login = ()=>{
     $scope.errorData = {};
@@ -27,5 +32,45 @@ app.controller('loginController', function($scope, apiInterface, snackbar, userI
       $scope.loggingIn = false;
     }
     apiInterface.post('login', $scope.loginData, {}, successCallback, errorCallback);
+  }
+
+  $scope.save = function(){
+    $scope.saving = true;
+    let success = response => {
+      $scope.saving = false;
+      if(response.status == 200){
+        $scope.saving = true;
+        snackbar.green('Usuario registrado.');
+        $scope.form.$setPristine();
+        $timeout(() => {
+          window.location.href = '/#!/login';
+        }, 1500);
+      }
+    }
+    let error = response=>{
+      $scope.saving = false;
+      if(response.status == 422) {
+        $scope.formErrors = response.data;
+        // updateFormValidation();
+      }
+      else{
+        snackbar.red('Se presentó un error al guardar.');
+      }
+      $scope.saving = false;
+    };
+    if($scope.user.id){
+      
+    }
+    else{
+      apiInterface.post('usuario', $scope.user, {}, success, error);
+    }
+  }
+  function updateFormValidation(){
+    const keys = Object.keys($scope.formErrors);
+    keys.forEach(k=>{
+      $scope.form[k].$setValidity('unique', false);
+      $scope.form[k].$setPristine();
+      
+    })
   }
 });
